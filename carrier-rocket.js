7d41356bc090eaf66dd5898c4f2c4d28dd94e5ec
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-04-18 00:49:06 +0300

    feat: add solutions

--- carrier-rocket.js
+++ carrier-rocket.js
@@ -1,9 +1,11 @@
 class CarrierRocket {
   constructor(stages) {
-    
+    this.stages = [...stages];
   }
 
-
+  detachStage() {
+    this.stages.splice(0, 1);
+  }
 }
 
 module.exports = CarrierRocket;
